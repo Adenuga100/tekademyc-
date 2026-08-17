bc63f9fbe6ae059670eed9710d145dcca0c90007
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-08-17 14:06:51 +0100

    library

--- PageObjects/Tekademypage.cs
+++ PageObjects/Tekademypage.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 //namespace Tekademy1C.PageObjects
 //{
@@ -44,7 +45,47 @@ namespace Tekademy1C.PageObjects
         {
             return _page.Locator("input[type='checkbox']");
         }
-        
+
+        public ILocator menu(string menus)
+        {
+            return _page.Locator($"span:has-text('{menus}')");
+        }
+
+        public ILocator select()
+        {
+            return _page.Locator("select");
+        }
+
+        public ILocator buttons(string button)
+        {
+            return _page.GetByRole(AriaRole.Button, new() { Name = button });
+        }
+
+        public ILocator enterTitle()
+        {
+            return _page.Locator("input[id='resource-title']");
+        }
+
+        public ILocator unCheckPublishImmediately()
+        {
+            return _page.Locator("input[type='checkbox']");
+        }
+
+        public ILocator selectFileAsync()
+        {
+            return _page.Locator("input[type='file']");
+        }
+
+        public ILocator enterTags()
+        {
+            return _page.Locator("input[id='resource-tags']");
+        }
+
+        public ILocator enterDescriptions()
+        {
+            return _page.Locator("textarea[id='resource-description']");
+        }
+
         public ILocator dashboard()
         {
             return _page.Locator("img[alt='Tekademy']");
@@ -53,6 +94,19 @@ namespace Tekademy1C.PageObjects
         public ILocator displayed(string message)
         {
             return _page.GetByText(message).First;
+        }
+
+        public string GenerateRandomName()
+        {
+            string[] categories = { "Dev", "Tester", "QA"};
+           
+
+            Random random = new Random();
+
+            string category = categories[random.Next(categories.Length)];
+          
+
+            return $"{category}";
         }
 
         public async Task OpenAsync(string url)
@@ -79,7 +133,51 @@ namespace Tekademy1C.PageObjects
         {
             await checkBox().ClickAsync();
         }
-       
+
+        public async Task clickMenus(string menus)
+        {
+            await menu(menus).ClickAsync();
+        }
+
+        public async Task selectCategory()
+        {
+            string category = GenerateRandomName();
+            await select().SelectOptionAsync(category);
+        }
+
+        public async Task clickButton(string button)
+        {
+            await buttons(button).ClickAsync();
+        }
+
+        public async Task enterTitleAsync(string title)
+        {
+            await enterTitle().FillAsync(title);
+        }
+
+        public async Task unCheckPublishImmediatelyAsync()
+        {
+            await unCheckPublishImmediately().ClickAsync();
+        }
+
+        public async Task selectFileAsync(string filePath)
+        {
+            await selectFileAsync().SetInputFilesAsync(filePath);
+        }
+      
+        public async Task enterTagsAsync(string tags)
+        {
+            await enterTags().FillAsync(tags);
+        }
+        
+
+        public async Task enterDescription(string title)
+        {
+            await enterDescriptions().FillAsync(title);
+        }
+
+      
+
         public async Task Dashboard()
         {
             await dashboard().WaitForAsync();
