3525f68cdfbe4d09f33509f446d6a087199fe961
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-08-18 13:19:12 +0100

    fix: untrack build files

--- obj/Debug/net8.0/Tekademy1C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tekademy1C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9581c164d7dc1aab788f979c4624ab2f310b62a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d001a26e649706e97a70d7df5e5ea79f0808f4c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
