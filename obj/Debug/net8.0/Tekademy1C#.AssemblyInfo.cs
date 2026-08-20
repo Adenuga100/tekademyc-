8472a54a6a80823f096cdd45d906d38629c2e339
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-08-20 07:36:37 +0100

    Initial Playwright BDD framework setup

--- obj/Debug/net8.0/Tekademy1C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tekademy1C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22b39a43908f85e608cf2419952121fa08b1f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20d5dcbe4242cc00ad08b9a7f814c2fdb5dfc85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tekademy1C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
